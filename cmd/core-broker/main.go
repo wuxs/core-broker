1405ddc7822e6c0c039a6feec5b8e9cef83b85bf
liuzhen <liuzhen@yunify.com>
2021-12-15 16:40:57 +0800

    feat: add license

--- cmd/core-broker/main.go
+++ cmd/core-broker/main.go
@@ -1,11 +1,18 @@
 /*
- * Filename: /Users/sc/liuzhen/core-broker/cmd/core-broker/main.go
- * Path: /Users/sc/liuzhen/core-broker/cmd/core-broker
- * Created Date: Thursday, December 9th 2021, 2:31:00 pm
- * Author: sc
- *
- * Copyright (c) 2021 Your Company
- */
+Copyright 2021 The tKeel Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package main
 

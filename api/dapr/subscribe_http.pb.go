301c8896ec51c4b1b7e640ed455700cc721ce252
uptutu <alexkung@outlook.com>
2022-02-15 18:26:30 +0800

    upgrade: kit

--- api/dapr/subscribe_http.pb.go
+++ api/dapr/subscribe_http.pb.go
@@ -57,39 +57,7 @@ func (h *SubscribeHTTPHandler) GetSubscribe(req *go_restful.Request, resp *go_re
 			result.Set(tErr.Reason, tErr.Message, out), "application/json")
 		return
 	}
-	anyOut, err := anypb.New(out)
-	if err != nil {
-		resp.WriteHeaderAndJson(http.StatusInternalServerError,
-			result.Set(errors.InternalError.Reason, err.Error(), nil), "application/json")
-		return
-	}
-
-	outB, err := protojson.MarshalOptions{
-		UseProtoNames:   true,
-		EmitUnpopulated: true,
-	}.Marshal(&result.Http{
-		Code: errors.Success.Reason,
-		Msg:  "",
-		Data: anyOut,
-	})
-	if err != nil {
-		resp.WriteHeaderAndJson(http.StatusInternalServerError,
-			result.Set(errors.InternalError.Reason, err.Error(), nil), "application/json")
-		return
-	}
-	resp.AddHeader(go_restful.HEADER_ContentType, "application/json")
-
-	var remain int
-	for {
-		outB = outB[remain:]
-		remain, err = resp.Write(outB)
-		if err != nil {
-			return
-		}
-		if remain == 0 {
-			break
-		}
-	}
+	resp.WriteAsJson(out.GetSubscriptions())
 }
 
 func RegisterSubscribeHTTPServer(container *go_restful.Container, srv SubscribeHTTPServer) {
